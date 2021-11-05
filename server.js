a2de5638a3f6779fed7b15ca31aa392a84c253e6
starshooter12345 <80570233+starshooter12345@users.noreply.github.com>
2021-11-05 11:20:23 +0530

    second

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ const app = express();
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//added comment
