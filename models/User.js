14b6afbd366b5878eca8268821553a0614cd65c1
iamdevopslearner <humbleandwise16@gmail.com>
2021-11-18 00:44:35 +0530

    Create and update profile routes

--- models/User.js
+++ models/User.js
@@ -22,4 +22,4 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('user', UserSchema);
