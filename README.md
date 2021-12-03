b4e1d59e64fa3f58a7f4efce83e433667f315dbd
starshooter12345 <humbleandwise16@gmail.com>
2021-12-03 10:33:18 +0530

    Modified README.md

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # DevConnector
 
 This is a MERN stack social media application that includes authentication, profiles and forum posts.
+
+1)Clone the project
+
+2. In the root of the project, type npm install in the VS code terminal (to install dependencies)
+
+3)Type cd client to go inside the client folder
+
+4. Then tpe npm install again
+
+5. From the root tpe npm run dev to start the backend server and development (react) server.
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+const Register = () =>{
 
-const Register = () => {
-  return (
-    <div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <p />
-      Register
-    </div>
-  );
-};
+  const [formData, setFormData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    password2:''
+  })
+  const{name, email, password, password2}=formData;
+
+
+  return <Fragment>
+    <br/><br/><br/>
+    <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i class="fas fa-user"></i> Create Your Account</p>
+      <form className="form" action="create-profile.html">
+        <div className="form-group">
+          <input type="text" placeholder="Name" name="name" value={name} required />
+        </div>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <a href="login.html">Sign In</a>
+      </p>
+
+  </Fragment>
+}
 export default Register;
