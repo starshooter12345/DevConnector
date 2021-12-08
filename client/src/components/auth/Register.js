74864c397c4bb994df9dc22cd81a0d41be62b567
starshooter12345 <humbleandwise16@gmail.com>
2021-12-08 19:31:37 +0530

    Auth reducer and register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-import {setAlert} from '../../actions/alert';
+import { setAlert } from '../../actions/alert';
+
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
-const Register = ({setAlert}) => {
+const Register = ({ setAlert,register }) => {
 
   const [formData, setFormData] = useState({
     name:'',
@@ -20,7 +22,7 @@ const Register = ({setAlert}) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('success');
+      register({name,email,password});
     }
   };
  return(
@@ -30,10 +32,10 @@ const Register = ({setAlert}) => {
       <p className="lead"><i class="fas fa-user"></i> Create Your Account</p>
       <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
-          <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} required />
+          <input type="text" placeholder="Name" name="name" value={name} onChange={onChange}  />
         </div>
         <div className="form-group">
-          <input type="email" placeholder="Email Address" name="email" value={email} onChange ={onChange} required />
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange ={onChange}  />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -46,7 +48,7 @@ const Register = ({setAlert}) => {
             name="password"
             value={password}
             onChange ={onChange}
-            minLength="6"
+            
           />
         </div>
         <div className="form-group">
@@ -56,7 +58,7 @@ const Register = ({setAlert}) => {
             name="password2"
             value={password2}
             onChange ={onChange}
-            minLength="6"
+            
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -70,5 +72,6 @@ const Register = ({setAlert}) => {
  }
  Register.propTypes={
    setAlert:PropTypes.func.isRequired,
+   register:PropTypes.func.isRequired,
  }
-export default connect(null,{setAlert})(Register);
+export default connect(null,{setAlert, register})(Register);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -6,9 +6,10 @@ export default combineReducers({
 });*/
 import { combineReducers } from 'redux';
 import alert from './alert';
-
+import auth from './auth';
 
 export default combineReducers({
   alert,
+  auth
   
 });
