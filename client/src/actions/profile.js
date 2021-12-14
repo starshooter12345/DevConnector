b3e33171b57d619831ee7c26fa86351da219b157
starshooter12345 <humbleandwise16@gmail.com>
2021-12-14 16:43:26 +0530

    Finished the Edit Profile action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -26,3 +26,68 @@ export const getCurrentProfile = () => async (dispatch) => {
 
     }
 }
+
+//Create or update profile
+/*export const createProfile = (formData, navigate , edit = false) => async (dispatch) => {
+    try{
+        const res = await axios.post('/api/profile', formData, config)
+    }
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert(edit ? 'Profile Updated ' : 'Profile created'))
+
+        if(!edit){
+            history.push('/dashboard')
+        }
+
+    }catch(err){
+        const errors= err.response.data.errors;
+        if(errors){
+            errors.forEach((error) => dispatch(setAlert(error.msg,'danger')));
+
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg : err.response.statusText, status: err.response.status }
+        })
+
+
+    }
+} */
+//Create or update profile
+export const createProfile =
+  (formData, navigate, edit = false) =>
+  async (dispatch) => {
+    try {
+      const res = await axios.post('/api/profile', formData);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+
+      dispatch(
+        setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success')
+      );
+
+      if (!edit) {
+        navigate('/dashboard');
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  };
+
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,10 +1,28 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState , useEffect} from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Link, useMatch, useNavigate} from 'react-router-dom'
+import { createProfile, getCurrentProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const initialState = {
+        company: '',
+        website: '',
+        location: '',
+        status: '',
+        skills: '',
+        githubusername: '',
+        bio: '',
+        twitter: '',
+        facebook: '',
+        linkedin: '',
+        youtube: '',
+        instagram: ''
 
-    const [formData, setFormData] = useState({
+}
+
+const CreateProfile = ({ profile: { profile, loading }, createProfile, getCurrentProfile })=> {
+
+    /*const [formData, setFormData] = useState({
 
         company: '',
         website: '',
@@ -19,9 +37,34 @@ const CreateProfile = props => {
         youtube: '',
         instagram: ''
 
-    });
+    });*/
+    const [formData, setFormData] = useState(initialState);
+    const creatingProfile = useMatch('/create-profile');
 
     const [displaySocialInputs, toggleSocialInputs] = useState(false);
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      if(!profile) getCurrentProfile();
+
+      if(!loading && profile){
+        const profileData = {...initialState};
+        for(const key in profile){
+          if( key in profileData) profileData[key] = profile[key];
+
+        }
+        for (const key in profile.social){
+          if(key in profileData) profileData[key] = profile.social[key];
+        }
+        //the skills may be an array from our API response
+        if(Array.isArray(profileData.skills))
+        profileData.skills = profileData.skills.join(', ');
+        //set local state with the profileData
+        setFormData(profileData);
+      }
+
+    }, [loading, getCurrentProfile, profile]);
     
     const {
         company,
@@ -38,20 +81,32 @@ const CreateProfile = props => {
         instagram
     } = formData;
 
+    const onChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = (e) => {
+      e.preventDefault();
+      createProfile(formData, navigate, profile ? true : false);
+    }
+
     return (
         <section className = "container">
             <h1 className="large text-primary">
-        Create Your Profile
+        { creatingProfile ? 'Create your profile' : 'Edit your profile'}
       </h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Let's get some information to make your
-        profile stand out
+        <i className="fas fa-user" />
+        {creatingProfile
+          ? ` Let's get some information to make your`
+          : ' Add some changes to your profile'}
+        
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
-          <select name="status">
-            <option value="0">* Select Professional Status</option>
+          <select name="status" value={status} onChange={onChange}>
+            <option>* Select Professional Status</option>
             <option value="Developer">Developer</option>
             <option value="Junior Developer">Junior Developer</option>
             <option value="Senior Developer">Senior Developer</option>
@@ -66,25 +121,25 @@ const CreateProfile = props => {
           >
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Company" name="company" />
+          <input type="text" placeholder="Company" name="company" value={company} onChange={onChange}/>
           <small className="form-text"
             >Could be your own company or one you work for</small
           >
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Website" name="website" />
+          <input type="text" placeholder="Website" name="website" value={website} onChange={onChange} />
           <small className="form-text"
             >Could be your own or a company website</small
           >
         </div>
         <div className="form-group">
-          <input type="text" placeholder="Location" name="location" />
+          <input type="text" placeholder="Location" name="location" value={location} onChange={onChange} />
           <small className="form-text"
             >City & state suggested (eg. Boston, MA)</small
           >
         </div>
         <div className="form-group">
-          <input type="text" placeholder="* Skills" name="skills" />
+          <input type="text" placeholder="* Skills" name="skills" value={skills} onChange={onChange} />
           <small className="form-text"
             >Please use comma separated values (eg.
             HTML,CSS,JavaScript,PHP)</small
@@ -95,6 +150,7 @@ const CreateProfile = props => {
             type="text"
             placeholder="Github Username"
             name="githubusername"
+            value={githubusername} onChange={onChange}
           />
           <small className="form-text"
             >If you want your latest repos and a Github link, include your
@@ -102,43 +158,49 @@ const CreateProfile = props => {
           >
         </div>
         <div className="form-group">
-          <textarea placeholder="A short bio of yourself" name="bio"></textarea>
+          <textarea placeholder="A short bio of yourself" name="bio" value={bio} onChange={onChange}></textarea>
           <small className="form-text">Tell us a little about yourself</small>
         </div>
 
         <div className="my-2">
-          <button type="button" className="btn btn-light">
+          <button  onClick={() => toggleSocialInputs(!displaySocialInputs)} type="button" className="btn btn-light">
             Add Social Network Links
           </button>
           <span>Optional</span>
         </div>
 
-        <div className="form-group social-input">
+        {displaySocialInputs && (<Fragment>
+          <div className="form-group social-input">
           <i className="fab fa-twitter fa-2x"></i>
-          <input type="text" placeholder="Twitter URL" name="twitter" />
+          <input type="text" placeholder="Twitter URL" name="twitter" value={twitter} onChange={onChange} />
         </div>
 
         <div className="form-group social-input">
           <i className="fab fa-facebook fa-2x"></i>
-          <input type="text" placeholder="Facebook URL" name="facebook" />
+          <input type="text" placeholder="Facebook URL" name="facebook" value={facebook} onChange={onChange}/>
         </div>
 
         <div className="form-group social-input">
           <i className="fab fa-youtube fa-2x"></i>
-          <input type="text" placeholder="YouTube URL" name="youtube" />
+          <input type="text" placeholder="YouTube URL" name="youtube" value={youtube} onChange={onChange}/>
         </div>
 
         <div className="form-group social-input">
           <i className="fab fa-linkedin fa-2x"></i>
-          <input type="text" placeholder="Linkedin URL" name="linkedin" />
+          <input type="text" placeholder="Linkedin URL" name="linkedin" value={linkedin} onChange={onChange} />
         </div>
 
         <div className="form-group social-input">
           <i className="fab fa-instagram fa-2x"></i>
-          <input type="text" placeholder="Instagram URL" name="instagram" />
+          <input type="text" placeholder="Instagram URL" name="instagram" value={instagram} onChange={onChange} />
         </div>
+          
+          
+        </Fragment> )}
+
+        
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">Go Back</a>
+        <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
       </form>
 
         </section>
@@ -146,7 +208,17 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+   createProfile: PropTypes.func.isRequired,
+   getCurrentProfile: PropTypes.func.isRequired,
+   profile: PropTypes.object.isRequired
 }
 
-export default CreateProfile
+const mapStateToProps = (state) =>({
+  profile: state.profile
+})
+
+
+
+export default connect(mapStateToProps, { createProfile, getCurrentProfile}) (
+  CreateProfile
+)
