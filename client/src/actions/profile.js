156771f0751037ac7d27f31021805d7e498e3524
starshooter12345 <humbleandwise16@gmail.com>
2021-12-26 22:05:19 +0530

    Delete experience done

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,8 @@ import { setAlert } from './alert';
 
 
 import{
+  ACCOUNT_DELETED,
+  CLEAR_PROFILE,
     GET_PROFILE,
     PROFILE_ERROR,
     UPDATE_PROFILE
@@ -140,6 +142,24 @@ export const addEducation = (formData, navigate) => async (dispatch) => {
   }
 };
 
+//Delete experience
+/*export const deleteExperience = id => async dispatch => {
+  try{
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type:UPDATE_PROFILE,
+      payload:res.data
+    })
+    dispatch(setAlert('Experience Removed','success'));
+  }catch(err){
+    dispatch({
+      type:PROFILE_ERROR,
+      payload:{ msg: err.response.statusText, status:err.response.status }
+    })
+  }
+
+}*/
 // Delete experience
 export const deleteExperience = (id) => async (dispatch) => {
   try {
@@ -152,15 +172,69 @@ export const deleteExperience = (id) => async (dispatch) => {
 
     dispatch(setAlert('Experience Removed', 'success'));
   } catch (err) {
-    const errors = err.response.data.errors;
-
-    if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-    }
-
     dispatch({
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status }
     });
   }
 };
+
+
+//Delete education
+export const deleteEducation = id => async dispatch => {
+  try{
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type:UPDATE_PROFILE,
+      payload:res.data
+    })
+    dispatch(setAlert('Education Removed','success'));
+  }catch(err){
+    dispatch({
+      type:PROFILE_ERROR,
+      payload:{ msg: err.response.statusText, status:err.response.status }
+    })
+  }
+
+}
+/*
+//Delete account and profile
+export const deleteAccount = () => async dispatch => {
+  if(window.confirm('Are you sure? This cannot be undone!')){
+    try{
+      const res = await axios.delete('/api/profile');
+
+      dispatch({ type: CLEAR_PROFILE});
+      dispatch({ type: ACCOUNT_DELETED});
+
+      dispatch(setAlert('Ypur account has been permanently deleted','success'));
+    }catch(err){
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status}
+      })
+  }
+  }
+}*/
+// Delete account & profile
+export const deleteAccount = () => async (dispatch) => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      await axios.delete('/api/profile');
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert('Your account has been permanently deleted'));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
+  }
+};
+
+
+
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 //import { deleteExperience } from '../../actions/profile';
 import formatDate from '../../utils/formatDate';
+import { deleteExperience } from '../../actions/profile';
 
-const Experience = ({ experience }) => {
+const Experience = ({ experience, deleteExperience }) => {
   const experiences = experience.map((exp) => (
     <tr key={exp._id}>
       <td>{exp.company}</td>
@@ -13,7 +14,7 @@ const Experience = ({ experience }) => {
         {formatDate(exp.from)} - {exp.to ? formatDate(exp.to) : 'Now'}
       </td>
       <td>
-        <button
+        <button onClick={() => deleteExperience(exp._id)} 
         
           className="btn btn-danger"
         >
@@ -43,7 +44,8 @@ const Experience = ({ experience }) => {
 
 Experience.propTypes = {
   experience: PropTypes.array.isRequired,
+  deleteExperience: PropTypes.func.isRequired,
  
 };
 
-export default Experience;
+export default connect(null, { deleteExperience})(Experience);
