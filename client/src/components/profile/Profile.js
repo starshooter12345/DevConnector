bb1bf7f0027c56144871e158d1eeaf6f8a20b0d0
starshooter12345 <humbleandwise16@gmail.com>
2022-01-06 18:56:51 +0530

    Finished profile top

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -10,61 +10,12 @@ import PropTypes from 'prop-types';
 import { Link, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileTop from './ProfileTop'
+import ProfileAbout from './ProfileAbout'
+import ProfileExperience from './ProfileExperience'
 import { getProfileById } from '../../actions/profile';
 
-
-/*const Profile = ({getProfileById, profile: { profile }, auth}) => {
-    const { id } = useParams();
-    useEffect(() => {
-        getProfileById(id);
-
-    }, [getProfileById, id])
-    return (
-        
-      <section className="container">
-          {profile === null || auth.loading ? ( <Spinner/> ): (<Fragment> 
-              <Link to='/profiles' className='btn btn-light'>Back to Profiles</Link>
-              {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id
-              &&(<Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>)}
-              
-              </Fragment>)}
-      </section>
-    )
-}*/
-
-/*
-//manually typed
-const Profile = ({ getProfileById, profile: { profile }, auth}) => {
-  const { id } = useParams();
-
-  
-    useEffect(() => {
-      getProfileById( id );
-    }, [getProfileById, id]);
-  
-    return (
-      <section className="container">
-        {profile === null  ? (
-          <Spinner />
-        ) : (
-          <Fragment>
-            <Link to="/profiles" className="btn btn-light">
-              Back To Profiles
-            </Link>
-            {auth.isAuthenticated &&
-              auth.loading === false &&
-              auth.user._id === profile.user._id && (
-                <Link to="/edit-profile" className="btn btn-dark">
-                  Edit Profile
-                </Link>
-              )}
-        
-          </Fragment>
-        )}
-      </section>
-    )
-    
-}*/
+//best manually typed
 
 const Profile = ({ getProfileById, profile: { profile }, auth }) => {
   const { id } = useParams();
@@ -88,58 +39,45 @@ const Profile = ({ getProfileById, profile: { profile }, auth }) => {
                 Edit Profile
               </Link>
             )}
-          
-                </Fragment>    
+
+            <div className="profile-grid my-1">
+              <ProfileTop profile={profile} />
+              <ProfileAbout profile={profile}/>
+              <div className="profile-exp bg-white p-2">
+                <h2 className="text-primary">Experience</h2>
+                {profile.experience.length > 0 ? (
+                  <Fragment>  
+                {profile.experience.map((experience) => (
+                  <ProfileExperience key={experience._id} experience={experience}/>
+                ))}
+                </Fragment>
+                ) : (
+                  <h4>No experience credentials</h4>
+                )}
+              </div>
+              </div>
+          </Fragment>    
       )}
     </section>
   );
 };
 
-/*
-const Profile = ({ getProfileById, profile: { profile }, auth }) => {
-    const { id } = useParams();
-    useEffect(() => {
-      getProfileById(id);
-    }, [getProfileById, id]);
-  
-    return (
-      <section className="container">
-        {profile === null ? (
-          <Spinner />
-        ) : (
-          <Fragment>
-            <Link to='/profiles' className="btn btn-light">
-              Back To Profiles
-            </Link>
-            {auth.isAuthenticated &&
-              auth.loading === false &&
-              auth.user._id === profile.user._id && (
-                <Link to='/edit-profile' className="btn btn-dark">
-                  Edit Profile
-                </Link>
-              )}
-        
-  
-              
-  
-             
-          </Fragment>
-        )}
-      </section>
-    );
-  };
-    */
-
 
 Profile.propTypes = {
-    getProfileById: PropTypes.func.isRequired,
-    profile:PropTypes.object.isRequired,
-    auth:PropTypes.object.isRequired,
+  getProfileById: PropTypes.func.isRequired,
+  profile:PropTypes.object.isRequired,
+  auth:PropTypes.object.isRequired,
 
 }
 const mapStateToProps = state => ({
-    profile:state.profile,
-    auth:state.auth
+  profile:state.profile,
+  auth:state.auth
 })
 
 export default connect(mapStateToProps, {getProfileById})(Profile)
+
+
+
+
+
+
