1476c0edadb4841405c479735aad218e3b7a45ba
starshooter12345 <humbleandwise16@gmail.com>
2022-01-07 21:15:35 +0530

    Final push

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -35,7 +35,12 @@ export default postReducer;*/
 
 import {
     GET_POSTS,
+    GET_POST,
+    DELETE_POST,
+    ADD_POST,
     POST_ERROR,
+    ADD_COMMENT,
+    REMOVE_COMMENT,
     UPDATE_LIKES,
 
     
@@ -59,7 +64,24 @@ import {
           posts: payload,
           loading: false
         };
-     
+      case GET_POST:
+          return {
+            ...state,
+            post: payload,
+            loading: false
+          };
+      case ADD_POST:
+            return {
+              ...state,
+              posts: [payload, ...state.posts],
+              loading: false
+            };
+      case DELETE_POST:
+              return {
+                ...state,
+                posts: state.posts.filter((post) => post._id !== payload),
+                loading: false
+              };
       case POST_ERROR:
         return {
           ...state,
@@ -72,6 +94,23 @@ import {
           posts: state.posts.map((post) => post._id === payload.id ? { ...post, likes: payload.likes} : post),
           loading:false
         }
+      case ADD_COMMENT:
+          return {
+            ...state,
+            post: { ...state.post, comments: payload },
+            loading: false
+          };
+      case REMOVE_COMMENT:
+          return {
+            ...state,
+            post: {
+              ...state.post,
+              comments: state.post.comments.filter(
+                (comment) => comment._id !== payload
+              )
+            },
+            loading: false
+          };
     
       default:
         return state;
